fe7aa9e273ab8941f4fa0e055eeaae1730304efd
Patrick Edwards <pat.e1@icloud.com>
2022-08-20 21:35:09 +1000

    trying rel preload

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -62,7 +62,7 @@ export default {
                         <h1 class='modalTitle font-bold'>
                             <slot name="title">Oops...</slot>
                         </h1>
-                        <img v-if="this.showImage" class='modalImage' :src="getRandomImage(this.category)"
+                        <img v-if="this.showImage" class='modalImage' :src="getRandomImage(this.category)" rel="preload"
                             alt='Hikaru absolutely disgusted' />
                         <p class='modalBody font-Sen p-5'>
                             <slot name="body">Something went wrong :(</slot>
