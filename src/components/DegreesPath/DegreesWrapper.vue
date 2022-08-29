5381970dc0f8f2cf6bc095dd3956d1a05d752197
Patrick Edwards <pat.e1@icloud.com>
2022-08-30 00:44:53 +1000

    use live cloud function

--- src/components/DegreesPath/DegreesWrapper.vue
+++ src/components/DegreesPath/DegreesWrapper.vue
@@ -200,8 +200,8 @@ export default {
         async fetchBestWin(username, timeControl, requestAttemps) {
             if (requestAttemps !== 0 && username) {
                 try {
-                    // const res = await fetch("https://us-central1-six-degrees-of-hikaru-cf099.cloudfunctions.net/scraper", {
-                    const res = await fetch("http://localhost:5001/six-degrees-of-hikaru-cf099/us-central1/scraper", {
+                    const res = await fetch("https://us-central1-six-degrees-of-hikaru-cf099.cloudfunctions.net/scraper", {
+                    // const res = await fetch("http://localhost:5001/six-degrees-of-hikaru-cf099/us-central1/scraper", {
                         method: 'POST',
                         body: JSON.stringify({
                             "text": `https://www.chess.com/stats/live/${timeControl}/${username}`
