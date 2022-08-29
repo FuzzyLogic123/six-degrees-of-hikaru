277e9495db55ca77a486c75641aeafd72e31c6fd
Patrick Edwards <pat.e1@icloud.com>
2022-08-29 22:42:59 +1000

    added correct imports

--- src/components/TotalPathsCalculated.vue
+++ src/components/TotalPathsCalculated.vue
@@ -1,4 +1,6 @@
 <script>
+import { onSnapshot } from "firebase/firestore";
+import { docRef } from '@/firebaseConfig.js';
 import gsap from 'gsap';
 import { authenticateUser, auth } from "../firebaseConfig";
 
