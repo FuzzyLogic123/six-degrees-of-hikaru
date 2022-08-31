0a7f2cacf518becf8670c371930ad979ddef9072
FuzzyLogic123 <pat.e1@icloud.com>
2022-09-01 06:56:58 +1000

    add link to reddit

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -15,7 +15,10 @@ export default {
         <h4 class='text-white font-bold font-RobotoCondensed text-center text-lg lg:text-2xl 2xl:text-3xl mx-5'>
             Patrick Edwards 2022 ©
         </h4>
-        <Reddit class=' mx-2 h-11' />
+        <a href="https://www.reddit.com/r/chess/comments/x30op1/i_made_a_website_to_find_the_chain_of_wins_from/"
+            target="_blank" class="cursor-pointer">
+            <Reddit class=' mx-2 h-11' />
+        </a>
         <a href="https://github.com/FuzzyLogic123/six-degrees-of-hikaru-vue" target="_blank" class="cursor-pointer">
             <Github class='mx-2 h-11' />
         </a>
