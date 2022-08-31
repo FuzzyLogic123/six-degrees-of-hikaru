2e4517bd1eaba286b4da4820c9dba6f7fd0ec057
FuzzyLogic123 <pat.e1@icloud.com>
2022-09-01 06:14:03 +1000

    hide quotes on smaller screens

--- src/components/Testimonial/Testimonial.vue
+++ src/components/Testimonial/Testimonial.vue
@@ -14,9 +14,9 @@ export default {
         <div
             class="relative bg-[#31415985] leading-8 p-4 pt-24 text-center text-white text-xl rounded-lg sm:max-w-[13rem] lg:max-w-sm 2xl:max-w-2xl mb-32 sm:mb-0 lg:m-6">
             <Quote
-                class='lg:hidden xl:block z-20 stroke-white stroke-2 fill-transparent opacity-90 absolute lg:-left-14 lg:-top-4 -left-10 top-5 h-14' />
+                class='sm:hidden xl:block z-20 stroke-white stroke-2 fill-transparent opacity-90 absolute lg:-left-14 lg:-top-4 -left-10 top-5 h-14' />
             <Quote
-                class='lg:hidden xl:block z-20 stroke-white stroke-2 fill-transparent opacity-90 absolute lg:-right-20 lg:top-44 -right-20 top-44 h-14 -scale-100 rotate-2' />
+                class='sm:hidden xl:block z-20 stroke-white stroke-2 fill-transparent opacity-90 absolute lg:-right-20 lg:bottom-4 -right-20 bottom-24 h-14 -scale-100 rotate-2' />
             <img :src="this.quote[2]" alt="The Pope"
                 class="absolute z-10 w-40 h-40 left-0 right-0 m-auto -top-24 rounded-full object-cover" />
             <p class="font-Outfit sm:text-sm md:text-lg lg:text-2xl">
